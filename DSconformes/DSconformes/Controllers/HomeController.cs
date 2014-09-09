c81517507013620f8ad7de542cbaff93b3c56441
smlflores18 <smlflores18@c58e42e7-5e85-2d88-4bd5-2d5fc8bc7e34>
2014-09-09 05:19:46 +0000

    Carga Inicial MVC

---
+++ DSconformes/DSconformes/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSconformes.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Reservación Restaurant";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ DSconformes/DSconformes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSconformes.MvcApplication" Language="C#" %>
