9256d85ce5a2bb506aee089f20ca820bc7c25725
cesar.argote.cueva@gmail.com <cesar.argote.cueva@gmail.com@c58e42e7-5e85-2d88-4bd5-2d5fc8bc7e34>
2014-09-18 05:29:37 +0000

    Creando Acceso a BD

---
+++ DSconformes/DSconformes/Dominio/Mesa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace DSconformes.Dominio
+{
+    [DataContract]
+    public class Mesa
+    {
+        [DataMember]
+        public int Codigo { get; set; }
+        [DataMember]
+        public Zona Zona { get; set; }
+        [DataMember]
+        public string Estado { get; set; }
+        [DataMember]
+        public int Capacidad { get; set; }
+
+    }
+}
---
+++ DSconformes/DSconformes/Dominio/Zona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace DSconformes.Dominio
+{
+    [DataContract]
+    public class Zona
+    {
+        [DataMember]
+        public int Codigo { get; set; }
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public string Descripcion { get; set; }
+        [DataMember]
+        public int Capacidad { get; set; }
+
+    }
+}
